4034617e78671203302bc58227b3a16305155736
Ty-Waltz <ty.waltz@yahoo.com>
2024-07-18 16:19:40 -0400

    first Python lesson

---
+++ Learning_python.py
@@ -0,0 +1,8 @@
+favorite_hobby = '''my favorite hobby would probably be playing video games. for me 
+its a great way to escape from the world for a bit and,
+in some way, express yourself in ways you cant in the real world'''
+
+print (favorite_hobby)
+#Coding Temple told me to do it :p
+sum = 4 + 2
+print ('Four plus two equals', sum)
