5c45bae7ee8c035c93eca46cdfe3e35980dc3b3a
Ty-Waltz <ty.waltz@yahoo.com>
2024-07-19 12:45:01 -0400

    NewExersises

---
+++ M2_L1_T3.py
@@ -0,0 +1,51 @@
+#exersise 2
+my_Name = "Ty"
+
+my_name = "Waltz"
+
+print(my_Name, my_name)
+
+#exersise 3
+fav_color = "purple"
+
+print(fav_color)
+
+#exersise 4
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+
+print(fruit1, fruit2, fruit3)
+
+#exersise 5
+my_integer = 5
+my_float = 1.5
+my_string = "String" 
+
+print(my_integer, my_float, my_string)
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+
+#exersise 6
+mysterey_variable = 1234
+print(type(mysterey_variable))
+
+#exersise 7 
+dynamic_var = 5 
+print(dynamic_var)
+
+dynamic_var = "five"
+print(dynamic_var)
+
+#exersise8 
+if = "not gon work"
+    
+
+#exersise 9
+PI = 3.14159
+
+#exersise 10
+my_var1, my_var2, my_var3 = 5, "car", 7.5
+print(type(my_var1))
+print(type(my_var2))
+print(type(my_var3))
+    
